5614039156e08e15ad7aecf4c1defc684af920e8
pannn <31822175+legokinder@users.noreply.github.com>
2024-06-10 00:01:52 +0900

    Update Q_backup.sh

--- Q_backup.sh
+++ Q_backup.sh
@@ -15,7 +15,6 @@ if [ ! -f "/root/.local/share/storj/uplink/config.yaml" ]; then
 fi
 
 # Define variables
-BACKUP_DIR="/root/backup"
 DIR_TO_BACKUP="/root/ceremonyclient/node/.config"
 VPS_IP=$(hostname -I | awk '{print $1}')
 STORJ_BUCKET="$VPS_IP"
@@ -23,27 +22,18 @@ STORJ_BUCKET="$VPS_IP"
 # Define backup file name with date and time
 BACKUP_FILE="backup_$(date +%Y%m%d_%H%M%S).tar.gz"
 
-# Create backup directory if it doesn't exist
-mkdir -p "$BACKUP_DIR"
+# Create a tar.gz file of the directory you want to backup and upload to Storj
+echo "Creating tar file of $DIR_TO_BACKUP and uploading to Storj..."
+tar -czf - -C "$(dirname "$DIR_TO_BACKUP")" "$(basename "$DIR_TO_BACKUP")" | uplink cp --stdin "sj://$STORJ_BUCKET/$BACKUP_FILE"
 
-# Create a tar.gz file of the directory you want to backup
-echo "Creating tar file of $DIR_TO_BACKUP..."
-tar -czf "$BACKUP_DIR/$BACKUP_FILE" -C "$(dirname "$DIR_TO_BACKUP")" "$(basename "$DIR_TO_BACKUP")"
+echo "Backup script execution completed."
 
-# Upload the backup file to Storj
-echo "Uploading $BACKUP_FILE to Storj..."
-uplink cp "$BACKUP_DIR/$BACKUP_FILE" "sj://$STORJ_BUCKET/$BACKUP_FILE"
-
-# Remove backups older than 10 days from the local backup directory
-echo "Removing local backups older than 10 days..."
-find "$BACKUP_DIR" -type f -name "*.tar.gz" -mtime +10 -exec rm -f {} \;
-
-# Optionally, remove backups older than 10 days from Storj
+# Remove backups older than 10 days from Storj
 OLD_DATE=$(date --date="10 days ago" +%Y%m%d)
 echo "Removing Storj backups older than 10 days..."
 uplink rm "sj://$STORJ_BUCKET/backup_$OLD_DATE*.tar.gz"
 
-echo "Backup script execution completed."
+echo "Storj backup deletion completed."
 
 # Function to set up cron job
 setup_cron_job() {
@@ -70,4 +60,3 @@ if ! crontab -l | grep -q "/root/Q_backup.sh"; then
     # Add the script to cron with the specified schedule
     setup_cron_job "$cron_schedule" "/root/Q_backup.sh"
 fi
-
