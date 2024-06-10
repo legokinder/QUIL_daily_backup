1872585fe9535a5b2bc4f51d90c85d11a2f07365
pannn <31822175+legokinder@users.noreply.github.com>
2024-06-10 13:25:41 +0900

    Update Q_backup.sh

--- Q_backup.sh
+++ Q_backup.sh
@@ -15,6 +15,7 @@ if ! command -v uplink &> /dev/null; then
     curl -L https://github.com/storj/storj/releases/latest/download/uplink_linux_amd64.zip -o uplink_linux_amd64.zip
     unzip -o uplink_linux_amd64.zip
     sudo install uplink /usr/local/bin/uplink
+    rm uplink_linux_amd64.zip
 fi
 
 # Storj setup (run only once)
